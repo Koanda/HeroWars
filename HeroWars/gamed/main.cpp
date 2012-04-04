763f3d76e860aad0f88211938f437b637eb20864
Intline9 <intline9@gmail.com>
2012-04-04 14:29:00 +0200

    Added GPLv3, as the code is open and free, but it should stay that way... so you can not fork and make it closed, and all bug fixes and improvements should be returned to us

--- HeroWars/gamed/main.cpp
+++ HeroWars/gamed/main.cpp
@@ -1,3 +1,20 @@
+/*HeroWars try's to by analoge to the League of Legends server
+Copyright (C) 2012  Intline9
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <stdio.h>
 #include <enet/enet.h>
 #include <intlib/base64.h>
