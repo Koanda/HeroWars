f74a58e6de45875e79811bcd5aba09a2f0d3c572
Intline9 <intline9@gmail.com>
2012-04-13 07:55:32 +0200

    Updated LoL version

--- HeroWars/RunClient.bat
+++ HeroWars/RunClient.bat
@@ -1,2 +1,2 @@
-cd "C:\Program Files (x86)\League of Legends\RADS\solutions\lol_game_client_sln\releases\0.0.0.134\deploy"
+cd "C:\Riot Games\League of Legends\rads\solutions\lol_game_client_sln\releases\0.0.0.140\deploy"
 "League of Legends.exe" "8390" "lol.launcher.exe" "Air\LOLClient.exe" "127.0.0.1 5109 17BLOhi6KZsTtldTsizvHg== 20164209"
--- HeroWars/RunClient_ltsstar.bat
+++
@@ -1,2 +0,0 @@
-cd "C:\Riot Games\League of Legends\rads\solutions\lol_game_client_sln\releases\0.0.0.140\deploy"
-"League of Legends.exe" "8390" "lol.launcher.exe" "Air\LOLClient.exe" "127.0.0.1 5109 17BLOhi6KZsTtldTsizvHg== 20164209"
